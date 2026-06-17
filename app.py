a94718e5ff4fa8aec44234bd418a35918b881acf
acalath1001-cmd <acalath.1001@gmail.com>
2026-06-17 11:58:38 +0900

    表示変更１

--- app.py
+++ app.py
@@ -1892,7 +1892,7 @@ def show_card(icon, title, subtitle, horse_text, bg_color, border_color, title_c
             ">
                 {horse_text}
             </div>
-        </div>
+        </div>n
         """,
         unsafe_allow_html=True
     )
